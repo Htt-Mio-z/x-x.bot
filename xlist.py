8665b03026e8d209e8015a6b92cfcf05ad75a85c
Nicozero <88931086+Nicozero@users.noreply.github.com>
2021-12-25 02:09:11 +0800

    Create xlist.py

---
+++ xlist.py
@@ -0,0 +1,36 @@
+linkpls = [
+
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072627818496010/Japan_Hits.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072628485410927/J-Pop_Powerplay.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072628313432064/J-Pop_Powerplay_Kawaii.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072628657356811/J-Pop_Sakura.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072628846112788/J-Rock_PowerPlay.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923072627973689344/J-Club_Powerplay_HipHop.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923477857823502396/Jazz_Sakura.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923477857622171648/Jazz_Club_Bandstand.pls' ,
+  'https://cdn.discordapp.com/attachments/613417127143014520/923477858012233748/J-Pop_Christmas_Radio.pls' ,
+
+]
+namepls = [
+  'Japan Hits.pls' ,
+	'J-Pop Powerplay.pls' ,
+	'J-Pop Powerplay Kawaii.pls' ,
+	'J-Pop Sakura.pls' ,
+  'J-Rock_PowerPlay' ,
+	'J-Club Powerplay HipHop.pls' ,
+	'Jazz Sakura.pls' ,
+	'Jazz Club Bandstand.pls' ,
+	'J-Pop Christmas Radio.pls' ,
+]
+
+sub = [
+  'jhits' ,
+	'jpop' ,
+	'jkawaii' ,
+	'jsakura' ,
+  'jrock' ,
+	'jclub' ,
+	'jazzs' ,
+	'jazz' ,
+	'jxmas' ,
+]
